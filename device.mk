be08c3ef72cc13a8472f60b5c91a0a93a2fede7e
Lunarixus <Nathan@lunarixus.dev>
2022-02-08 15:26:55 +0530

    vince: Remove unwanted GApps

--- device.mk
+++ device.mk
@@ -393,6 +393,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
