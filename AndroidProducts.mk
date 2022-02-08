f9c9956a7173d7d56d81ef9ef3fa8dfd7a838cf9
bhargavpalan <bhargav.bhargavpalan@gmail.com>
2022-02-08 15:10:25 +0530

    vince: Cherish-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_vince.mk
+    $(LOCAL_DIR)/cherish_vince.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_vince-user \
-    lineage_vince-userdebug \
-    lineage_vince-eng
+    cherish_vince-user \
+    cherish_vince-userdebug \
+    cherish_vince-eng
