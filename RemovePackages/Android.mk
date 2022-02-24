8f4e90dd7a3af86490967cec7b4dc1957e10bff3
bhargavpalan <bhargav.bhargavpalan@gmail.com>
2022-02-24 20:48:56 +0530

    vince: RemovePackage: remove some apps and re-arrange

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,45 +4,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := \
-    AmbientSensePrebuilt \
-    arcore \
-    Chrome \
-    Chrome-Stub \
-    ConnMO \
-    ConnMetrics \
-    DCMO \
-    DevicePolicyPrebuilt \
-    DMService \
-    GCS \
-    GoogleTTS \
-    Maps \
-    MyVerizonServices \
-    NexusLauncherRelease \
-    OBDM_Permissions \
-    obdm_stub \
-    OdadPrebuilt \
-    OemDmTrigger \
-    OPScreenRecord \
-    Ornament \
-    PixelBuds \
-    PixelLiveWallpaperPrebuilt \
-    RecorderPrebuilt \
-    SafetyHubPrebuilt \
-    SCONE \
-    ScribePrebuilt \
-    Showcase \
-    SoundAmplifierPrebuilt \
-    SprintDM \
-    SprintHM \
-    Tycho \
-    USCCDM \
-    Via \
-    Videos \
-    VZWAPNLib \
-    VzwOmaTrigger \
-    YouTube \
-    YouTubeMusicPrebuilt
+LOCAL_OVERRIDES_PACKAGES := Stk Drive GoogleTTS LocationHistoryPrebuilt MarkupGoogle Photos FilesPrebuilt
+LOCAL_OVERRIDES_PACKAGES += PrebuiltGmail talkback Videos YouTube YouTubeMusicPrebuilt DeskClock GoogleRestorePrebuilt
+LOCAL_OVERRIDES_PACKAGES += TurboPrebuilt Velvet WellbeingPrebuilt FM2 DevicePersonalizationPrebuiltPixel2021
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
