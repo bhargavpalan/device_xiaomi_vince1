c0a9538cbd37a9f710189df54d12536003617734
rezaadi0105 <rezaadipangestu5@gmail.com>
2022-03-13 21:10:34 +0530

    vince: prop: add tweak touch improve

--- prop.mk
+++ prop.mk
@@ -276,6 +276,25 @@ persist.sys.binary_xml=false
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.timed.enable=true
 
+# Touch improvements
+touch.pressure.scale=0.001
+persist.sys.ui.hw=1
+view.scroll_friction=10
+touch.size.calibration=diameter
+touch.size.scale=1
+touch.size.bias=0
+touch.size.isSummed=0
+touch.pressure.scale=0.001
+touch.orientation.calibration=none
+touch.distance.calibration=none
+touch.distance.scale=0
+touch.coverage.calibration=box
+touch.gestureMode=spots
+MultitouchSettleInterval=1ms
+MultitouchMinDistance=1px
+TapInterval=1ms
+TapSlop=1px
+
 # Tcp
 PRODUCT_PROPERTY_OVERRIDES += \
 net.tcp.2g_init_rwnd=10
